ba758b3b91731d4626124e371457180deefb63ab
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-11 18:33:18 +0530

    React Redux

--- reduxRouter/src/App.jsx
+++ reduxRouter/src/App.jsx
@@ -1,7 +1,14 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
+
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Learn about redux and redux Toolkit
     </h1>
+    <AddTodo />
+    <Todos />
+    </>
   )
 }
