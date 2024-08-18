d486673175f20dd888b650b967204f940aaa59d7
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-18 20:06:13 +0530

    Counter

--- Learning/src/App.jsx
+++ Learning/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { decrement, increment, reset } from './redux/slices/counter'
 
-export const App = () => {
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch()
+  console.log(count);
+  
   return (
-    <div>App</div>
+    <div>
+      <h1>React Redux Toolkit</h1>
+      <h1>Counter value {count}</h1>
+      <button onClick={()=>dispatch(increment())}>Increment</button><br />
+      <button onClick={()=>dispatch(decrement())}>Decrement</button><br />
+      <button onClick={()=>dispatch(reset())}>Reset</button>
+    </div>
   )
 }
+
+export default App
--- Learning/src/main.jsx
+++ Learning/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}> {/* poora app component k pass store ka access hai store mein reducer hai jo event handle krte hai  */}
     <App />
+    </Provider>
   </StrictMode>,
 )
